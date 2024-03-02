9df32af6cfa2a4e4544bfdafc0e974120a210f4b
Debdutta <m-debdutta@users.noreply.github.com>
2024-03-02 15:13:26 +0530

    WIP: working

--- src/instructions.js
+++ src/instructions.js
@@ -1,46 +1,48 @@
-const put = function (pc, program) {
-  const resultAddress = program[pc + 2];
-  program[resultAddress] = program[pc + 1];
-  return pc + 3;
-};
+class Instruction {
+  constructor() {}
+  put(pc, program) {
+    const resultAddress = program[pc + 2];
+    program[resultAddress] = program[pc + 1];
+    return pc + 3;
+  }
 
-const addition = function (pc, program) {
-  const resultAddress = program[pc + 3];
-  program[resultAddress] = program[pc + 1] + program[pc + 2];
-  return pc + 4;
-};
+  addition(pc, program) {
+    const resultAddress = program[pc + 3];
+    program[resultAddress] = program[pc + 1] + program[pc + 2];
+    return pc + 4;
+  }
 
-const substract = function (pc, program) {
-  const resultAddress = program[pc + 3];
-  program[resultAddress] = program[pc + 1] - program[pc + 2];
-  return pc + 4;
-};
+  substract(pc, program) {
+    const resultAddress = program[pc + 3];
+    program[resultAddress] = program[pc + 1] - program[pc + 2];
+    return pc + 4;
+  }
 
-const jump = function (pc, program) {
-  return program[pc + 1] - 1;
-};
+  jump(pc, program) {
+    return program[pc + 1] - 1;
+  }
 
-const jumpEqual = function (pc, program) {
-  const valueOfLocation1 = program[program[pc + 1]];
-  const valueOfLocation2 = program[program[pc + 2]];
-  return valueOfLocation1 === valueOfLocation2 ? program[pc + 3] : pc + 4;
-};
+  jumpEqual(pc, program) {
+    const valueOfLocation1 = program[program[pc + 1]];
+    const valueOfLocation2 = program[program[pc + 2]];
+    return valueOfLocation1 === valueOfLocation2 ? program[pc + 3] : pc + 4;
+  }
 
-const jumpLessThan = function (pc, program) {
-  const valueOfLocation1 = program[program[pc + 1]];
-  const valueOfLocation2 = program[program[pc + 2]];
-  return valueOfLocation1 < valueOfLocation2 ? program[pc + 3] : pc + 4;
-};
+  jumpLessThan(pc, program) {
+    const valueOfLocation1 = program[program[pc + 1]];
+    const valueOfLocation2 = program[program[pc + 2]];
+    return valueOfLocation1 < valueOfLocation2 ? program[pc + 3] : pc + 4;
+  }
 
-const copy = function (pc, program) {
-  program[program[pc + 2]] = program[program[pc + 1]];
-  return pc + 3;
-};
+  copy(pc, program) {
+    program[program[pc + 2]] = program[program[pc + 1]];
+    return pc + 3;
+  }
 
-const halt = function (pc, program) {
-  return pc;
-};
-
+  halt(pc, program) {
+    return pc;
+  }
+}
 exports[0] = put;
 exports[1] = addition;
 exports[2] = substract;
